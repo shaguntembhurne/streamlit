af57fc16c688ff08bb642c2aa8aadbcac82fe6d6
shaguntembhurne <shaxtembhurne@gmail.com>
2024-12-29 07:29:29 +0530

    Update p.py

--- p.py
+++ p.py
@@ -3,7 +3,6 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
-
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import HuggingFaceHub
 
